c7937ad2fcc0266d4baa7d658ae2ee01608c5b1f
Bikal <bikaladhikari5@gmail.com>
2024-05-10 17:49:04 +1000

    relevant css added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { useGlobalContext } from "../Context";
+import { FaBars } from "react-icons/fa";
 
 const Home = () => {
+  const { openSiderbar, openModal } = useGlobalContext();
   return (
-    <div>Home</div>
-  )
-}
+    <main>
+      <button onClick={openSiderbar} className="sidebar-toggle">
+        <FaBars />
+      </button>
+      <button onClick={openModal} className="btn">
+        show modal
+      </button>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
